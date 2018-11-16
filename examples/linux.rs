9ce8d106ce19059746f07babd79f3cfffad12bd3
Diego Barrios Romero <eldruin@gmail.com>
2018-11-16 17:36:15 +0100

    Extend linux example

--- examples/linux.rs
+++ examples/linux.rs
@@ -3,10 +3,22 @@ extern crate linux_embedded_hal;
 extern crate ds323x;
 
 use linux_embedded_hal::I2cdev;
-use ds323x::Ds323x;
+use ds323x::{ Ds323x, DateTime, Hours };
 
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let mut rtc = Ds323x::new_ds3231(dev);
-    println!("Seconds: {}", rtc.get_seconds().unwrap());
+    let datetime = DateTime {
+                              year: 2018,
+                              month: 08,
+                              day: 20,
+                              weekday: 4,
+                              hour: Hours::H24(19),
+                              minute: 59,
+                              second: 58
+                  };
+    rtc.set_datetime(&datetime).unwrap();
+    // do something else...
+    let seconds = rtc.get_seconds().unwrap();
+    println!("Seconds: {}", seconds);
 }
